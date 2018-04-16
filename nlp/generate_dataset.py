2a10c2999812c868099b32fccef71a6d650cfa88
Soros Liu <soros.liu1029@gmail.com>
2018-04-16 17:18:15 +0800

    Add nlp poc notebooks

--- nlp/generate_dataset.py
+++ nlp/generate_dataset.py
@@ -21,7 +21,7 @@ TRANS_TABLE = str.maketrans({
     '0': '零'
 })
 
-ABS_FORMATS = [
+ZH_ABS_FORMATS = [
     'short',
     'medium',
     'ah点m分',
@@ -34,7 +34,11 @@ ABS_FORMATS = [
     'H点mm分ss秒',
 ]
 
-REL_PREFIXS = {
+EN_ABS_FORMATS = [
+
+]
+
+ZH_REL_PREFIXS = {
     '今天': '+0D',
     '明天': '+1D',
     '后天': '+2D',
@@ -46,19 +50,23 @@ REL_PREFIXS = {
     '本周日': 'TW7',
     '下周日': 'NW7',
 
-    '?秒后': '+?S',
-    '?秒以后': '+?S',
-    '再过?秒': '+?S',
-    '?分钟后': '+?M',
-    '?分钟以后': '+?M',
-    '再过?分钟': '+?M',
-    '?小时后': '+?H',
-    '?小时以后': '+?H',
-    '再过?小时': '+?H',
+    '{}秒后': '+{}S',
+    '{}秒以后': '+{}S',
+    '再过{}秒': '+{}S',
+    '{}分钟后': '+{}M',
+    '{}分钟以后': '+{}M',
+    '再过{}分钟': '+{}M',
+    '{}小时后': '+{}H',
+    '{}小时以后': '+{}H',
+    '再过{}小时': '+{}H',
+}
+
+EN_REL_PREFIXS = {
+
 }
 
 for i in range(1, 8):
-    REL_PREFIXS.update({
+    ZH_REL_PREFIXS.update({
         '周{}'.format(str(i).translate(TRANS_TABLE)): 'TW{}'.format(i),
         '这周{}'.format(str(i).translate(TRANS_TABLE)): 'TW{}'.format(i),
         '本周{}'.format(str(i).translate(TRANS_TABLE)): 'TW{}'.format(i),
@@ -77,36 +85,46 @@ def translate_two_digits(digits):
         return '{}十{}'.format(tens, units)
 
 
-def generate_date():
+def generate_date(locale):
+    if locale == 'en':
+        locale = 'en_US'
+        formats = EN_ABS_FORMATS
+        relative = EN_REL_PREFIXS
+    else:
+        locale = 'zh_CN'
+        formats = ZH_ABS_FORMATS
+        relative = ZH_REL_PREFIXS
+
     dt = fake.future_datetime(end_date='+30d')
 
     machine_readable = dt.strftime('%H:%M:%S')
-    human_readable = format_time(dt, format=random.choice(ABS_FORMATS), locale='zh_CN')
+    human_readable = format_time(dt, format=random.choice(formats), locale=locale)
 
     r = random.random()
     if r < 0.3:
         machine_readable = 'ABS>' + machine_readable
     elif 0.3 <= r <= 0.7:
-        prefix = random.choice([k for k in REL_PREFIXS.keys() if '?' not in k])
-        machine_readable = REL_PREFIXS[prefix] + '>' + machine_readable
+        prefix = random.choice([k for k in relative.keys() if '{}' not in k])
+        machine_readable = relative[prefix] + '>' + machine_readable
         human_readable = prefix + human_readable
     else:
-        prefix = random.choice([k for k in REL_PREFIXS.keys() if '?' in k])
-        machine_readable = REL_PREFIXS[prefix]
-        human_readable = prefix
+        rand = random.randrange(start=1, stop=60)
+        prefix = random.choice([k for k in relative.keys() if '{}' in k])
+        machine_readable = relative[prefix].format(rand)
+        human_readable = prefix.format(rand)
 
     return human_readable, machine_readable, dt
 
 
-def generate_dataset(m):
+def generate_dataset(n, locale='zh'):
     lines = []
-    for i in range(m):
-        h, m, _ = generate_date()
-        lines.append(h + ',' + m + '\n')
+    for i in range(n):
+        h, m, _ = generate_date(locale)
+        lines.append(m.ljust(12) + ',' + h + '\n')
 
-    with open('dataset.txt', 'wt', encoding='utf-8') as f:
+    with open('dataset_{}_{}.txt'.format(locale, n), 'wt', encoding='utf-8') as f:
         f.writelines(lines)
 
 
 if __name__ == '__main__':
-    generate_dataset(int(1e3))
+    generate_dataset(int(1e5))
